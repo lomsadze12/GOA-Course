7d6d6ed0404868867f3861d0c4990c7c0db35df0
nerse12 <lomsadzegiorgi2323@gmail.com>
2024-11-28 22:04:34 +0400

    first commit

---
+++ lesson 01/classwork/cw.py
@@ -0,0 +1,58 @@
+# task 1
+name = "nerse"
+lastname = "lomsadze"
+age = "11"
+print(type(name))
+print(type(lastname))
+print(type(age))
+
+# task 2
+num1 = 4
+num2 = 6
+print(num1 + num2)
+
+# task 3
+#  თავიდან ცვლადს სახელი უნდა შევურჩიოთ შემდეგ დამაკავშირებელი
+#  ნიშანი ტოლობა და ბოლოს მნიშვნელობა
+#  რომელიც დაუკავშირდება სახელს მაგალითად city = "tbilisi"
+#  ცვლადს შესაბამისი სახელი იმიტომ უნდა დავარქვათ
+#  რომ ადვილად შევძლოთ მისი პოვნა რადგან ჩევენ თუ დავარქმევთ
+#  city - ს და შიგნით მანქწანის ბრენდს შევინახავთ შეიძლება ვერასდროს ვიპოვოთ
+
+# task 4
+print(6-2)
+print(7-4)
+print(62-23)
+print(26-11)
+print(5-1)
+
+print(6+2)
+print(6+24)
+print(63+24)
+print(62+22)
+print(96+214)
+
+print(62*2)
+print(6*231)
+print(63*221)
+print(64*253)
+print(6*24)
+
+print(62%2)
+print(6%231)
+print(63%221)
+print(64%253)
+print(364%4)
+
+print(62/2)
+print(6/231)
+print(63/221)
+print(64/253)
+print(364/4)
+
+
+
+
+
+
+
---
+++ lesson 03/classwork/cw.py
@@ -0,0 +1,18 @@
+#1
+name = input("enter your name: ")
+lastname = input("enter your lastname: ")
+age = input("enter your age: ")
+height = input("enter your height: ")
+weight = input("enter your weight: ")
+
+print(name)
+print(lastname)
+print(age)
+print(height)
+print(weight)
+
+#2
+age = int (input("enter your age: "))
+print(age + 50)
+
+
